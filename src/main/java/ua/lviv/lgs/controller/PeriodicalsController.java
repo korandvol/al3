1e7b270e66468415f0fdbdb4fb9065d8411b485b
korandvol <korandvol@gmail.com>
2021-07-31 10:16:36 +0300

    bucket and image upload lesson

---
+++ src/main/java/ua/lviv/lgs/controller/PeriodicalsController.java
@@ -0,0 +1,32 @@
+package ua.lviv.lgs.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import ua.lviv.lgs.domain.Periodical;
+import ua.lviv.lgs.service.PeriodicalsService;
+
+
+
+@Controller
+public class PeriodicalsController {
+
+	@Autowired
+	PeriodicalsService periodicalsService;
+
+	@RequestMapping(value = "/addPeriodical", method = RequestMethod.POST)
+	public ModelAndView createPeriodical(@Validated @ModelAttribute("periodical") Periodical periodical,
+			BindingResult bindingResult) {
+		periodicalsService.save(periodical);
+		return new ModelAndView("redirect:/home");
+	}
+
+}
---
+++ src/main/java/ua/lviv/lgs/service/PeriodicalsService.java
@@ -0,0 +1,29 @@
+package ua.lviv.lgs.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ua.lviv.lgs.dao.PeriodicalRepository;
+import ua.lviv.lgs.domain.Periodical;
+
+
+
+@Service
+public class PeriodicalsService {
+	
+	@Autowired
+	private PeriodicalRepository periodicalRepository;
+	
+	public Periodical save(Periodical periodical) {
+		return periodicalRepository.save(periodical);
+	}
+	
+	public List<Periodical> getAllPeriodicals(){
+		return periodicalRepository.findAll();
+	}
+	
+	
+	
+}
